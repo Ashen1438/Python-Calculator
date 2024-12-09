5141aab0a7244dad5c075138eb2ff00a82c32782
Ashen1438 <asheneranga1438@gmail.com>
2024-12-09 17:25:12 +0530

    new

---
+++ calculator.py
@@ -0,0 +1,32 @@
+from tkinter import Tk, Button, StringVar, Entry
+
+class Calculator:
+    def __init__(self, master):
+        master.title("Calculator")
+        master.geometry('357x420+0+0')
+        master.config(bg='gray')
+        master.resizable(False, False)
+
+        self.equation = StringVar()
+        self.entry_value = ''
+        Entry(width=17, bg='#fff', font=('Arial Bold', 28), textvariable=self.equation).place(x=0, y=0)
+
+    def show(self, value):
+        self.entry_value += str(value)
+        self.equation.set(self.entry_value)
+
+    def clear(self):
+        self.entry_value = ''
+        self.equation.set(self.entry_value)
+
+    def solve(self):
+        try:
+            result = eval(self.entry_value)
+            self.equation.set(result)
+        except Exception as e:
+            self.equation.set("Error")
+
+
+root = Tk()
+calculator = Calculator(root)
+root.mainloop()
